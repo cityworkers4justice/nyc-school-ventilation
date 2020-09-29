afad307fa1c449f90ea4a7ff69fb7d107ecad7ea
natalieoshea <ms.natalieoshea@gmail.com>
2020-09-29 13:31:12 -0400

    add bivariate mapping

--- global.R
+++ global.R
@@ -6,7 +6,8 @@ library(basemapR)
 # read in data
 ventilation_data <- read_csv("output/NYC_Schools_Ventilation_Data.csv")
 school_locations <- read_csv("data/NYC_School_Locations.csv")
-nta_data <- nycgeo::nyc_boundaries(geography = "nta", add_acs_data = TRUE)
+nta_data <- nycgeo::nyc_boundaries(geography = "nta", add_acs_data = TRUE) %>%
+  mutate(pop_nonwhite_pct_est = 1 - pop_white_pct_est)
 
 # rename columns
 names(ventilation_data) <- c("Primary_building_code","rooms_total","windows","windows_open", 
@@ -28,20 +29,20 @@ ventilation_data <- ventilation_data %>%
 
 # calculate percentages
 school_data <- ventilation_data %>%
-  mutate(perc_rooms_windows = round(windows/rooms_total * 100, digits = 1),
-         perc_rooms_windows_open = round(windows_open/rooms_total * 100, digits = 1),
-         perc_rooms_supplyFan = round(supplyFan_total/rooms_total * 100, digits = 1),
-         perc_rooms_supplyFan_operational = round(supplyFan_operational/rooms_total * 100, digits = 1),
-         perc_rooms_supplyFan_partOperational = round(supplyFan_partOperational/rooms_total * 100, digits = 1),
-         perc_rooms_supplyFan_nonOperational = round(supplyFan_nonOperational/rooms_total * 100, digits = 1),
-         perc_rooms_exhaustFan = round(exhaustFan_total/rooms_total * 100, digits = 1),
-         perc_rooms_exhaustFan_operational = round(exhaustFan_operational/rooms_total * 100, digits = 1),
-         perc_rooms_exhaustFan_partOperational = round(exhaustFan_partOperational/rooms_total * 100, digits = 1),
-         perc_rooms_exhaustFan_nonOperational = round(exhaustFan_nonOperational/rooms_total * 100, digits = 1),
-         perc_rooms_unitVentilator = round(unitVentilator_total/rooms_total * 100, digits = 1),
-         perc_rooms_unitVentilator_operational = round(unitVentilator_operational/rooms_total * 100, digits = 1),
-         perc_rooms_unitVentilator_partOperational = round(unitVentilator_partOperational/rooms_total * 100, digits = 1),
-         perc_rooms_unitVentilator_nonOperational = round(unitVentilator_nonOperational/rooms_total * 100, digits = 1))
+  mutate(perc_rooms_windows = round(windows/rooms_total, digits = 6),
+         perc_rooms_windows_open = round(windows_open/rooms_total, digits = 6),
+         perc_rooms_supplyFan = round(supplyFan_total/rooms_total, digits = 6),
+         perc_rooms_supplyFan_operational = round(supplyFan_operational/rooms_total, digits = 6),
+         perc_rooms_supplyFan_partOperational = round(supplyFan_partOperational/rooms_total, digits = 6),
+         perc_rooms_supplyFan_nonOperational = round(supplyFan_nonOperational/rooms_total, digits = 6),
+         perc_rooms_exhaustFan = round(exhaustFan_total/rooms_total, digits = 6),
+         perc_rooms_exhaustFan_operational = round(exhaustFan_operational/rooms_total, digits = 6),
+         perc_rooms_exhaustFan_partOperational = round(exhaustFan_partOperational/rooms_total, digits = 6),
+         perc_rooms_exhaustFan_nonOperational = round(exhaustFan_nonOperational/rooms_total, digits = 6),
+         perc_rooms_unitVentilator = round(unitVentilator_total/rooms_total, digits = 6),
+         perc_rooms_unitVentilator_operational = round(unitVentilator_operational/rooms_total, digits = 6),
+         perc_rooms_unitVentilator_partOperational = round(unitVentilator_partOperational/rooms_total, digits = 6),
+         perc_rooms_unitVentilator_nonOperational = round(unitVentilator_nonOperational/rooms_total, digits = 6))
 
 total_cols <- c("rooms_total","windows","windows_open",
                 "supplyFan_total","supplyFan_operational","supplyFan_partOperational","supplyFan_nonOperational",
@@ -56,19 +57,19 @@ district_data <- ventilation_data %>%
   mutate(across(all_of(total_cols), sum)) %>%
   unique() %>%
   ungroup() %>%
-  mutate(perc_rooms_windows = round(windows/rooms_total * 100, digits = 1),
-         perc_rooms_windows_open = round(windows_open/rooms_total * 100, digits = 1),
-         perc_rooms_supplyFan = round(supplyFan_total/rooms_total * 100, digits = 1),
-         perc_rooms_supplyFan_operational = round(supplyFan_operational/rooms_total * 100, digits = 1),
-         perc_rooms_supplyFan_partOperational = round(supplyFan_partOperational/rooms_total * 100, digits = 1),
-         perc_rooms_supplyFan_nonOperational = round(supplyFan_nonOperational/rooms_total * 100, digits = 1),
-         perc_rooms_exhaustFan = round(exhaustFan_total/rooms_total * 100, digits = 1),
-         perc_rooms_exhaustFan_operational = round(exhaustFan_operational/rooms_total * 100, digits = 1),
-         perc_rooms_exhaustFan_partOperational = round(exhaustFan_partOperational/rooms_total * 100, digits = 1),
-         perc_rooms_exhaustFan_nonOperational = round(exhaustFan_nonOperational/rooms_total * 100, digits = 1),
-         perc_rooms_unitVentilator = round(unitVentilator_total/rooms_total * 100, digits = 1),
-         perc_rooms_unitVentilator_operational = round(unitVentilator_operational/rooms_total * 100, digits = 1),
-         perc_rooms_unitVentilator_partOperational = round(unitVentilator_partOperational/rooms_total * 100, digits = 1),
-         perc_rooms_unitVentilator_nonOperational = round(unitVentilator_nonOperational/rooms_total * 100, digits = 1)) %>%
+  mutate(perc_rooms_windows = round(windows/rooms_total, digits = 6),
+         perc_rooms_windows_open = round(windows_open/rooms_total, digits = 6),
+         perc_rooms_supplyFan = round(supplyFan_total/rooms_total, digits = 6),
+         perc_rooms_supplyFan_operational = round(supplyFan_operational/rooms_total, digits = 6),
+         perc_rooms_supplyFan_partOperational = round(supplyFan_partOperational/rooms_total, digits = 6),
+         perc_rooms_supplyFan_nonOperational = round(supplyFan_nonOperational/rooms_total, digits = 6),
+         perc_rooms_exhaustFan = round(exhaustFan_total/rooms_total, digits = 6),
+         perc_rooms_exhaustFan_operational = round(exhaustFan_operational/rooms_total, digits = 6),
+         perc_rooms_exhaustFan_partOperational = round(exhaustFan_partOperational/rooms_total, digits = 6),
+         perc_rooms_exhaustFan_nonOperational = round(exhaustFan_nonOperational/rooms_total, digits = 6),
+         perc_rooms_unitVentilator = round(unitVentilator_total/rooms_total, digits = 6),
+         perc_rooms_unitVentilator_operational = round(unitVentilator_operational/rooms_total, digits = 6),
+         perc_rooms_unitVentilator_partOperational = round(unitVentilator_partOperational/rooms_total, digits = 6),
+         perc_rooms_unitVentilator_nonOperational = round(unitVentilator_nonOperational/rooms_total, digits = 6)) %>%
   # make park-cemetery data NA
   mutate(across(contains("perc_"), ~ifelse(str_detect(NTA_Name, "park-cemetery"), NA, .)))
